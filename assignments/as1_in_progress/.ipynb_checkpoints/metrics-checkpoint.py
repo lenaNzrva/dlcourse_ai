24716f08dc3c58e72332b9f276137da0026b16ca
Lena Lab Leptop <elena.nazarova@skoltech.ru>
2022-04-12 16:32:11 +0300

    first commit

---
+++ assignments/as1_in_progress/.ipynb_checkpoints/metrics-checkpoint.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+def binary_classification_metrics(prediction, ground_truth):
+    '''
+    Computes metrics for binary classification
+
+    Arguments:
+    prediction, np array of bool (num_samples) - model predictions
+    ground_truth, np array of bool (num_samples) - true labels
+
+    Returns:
+    precision, recall, f1, accuracy - classification metrics
+    '''
+    precision = 0
+    recall = 0
+    accuracy = 0
+    f1 = 0
+
+    # TODO: implement metrics!
+    # Some helpful links:
+    # https://en.wikipedia.org/wiki/Precision_and_recall
+    # https://en.wikipedia.org/wiki/F1_score
+    
+    TP = np.sum(prediction[ground_truth])
+    FP = np.sum(prediction[ground_truth == False])
+    FN = np.sum(ground_truth[prediction == False])
+    correct = np.sum(prediction[ground_truth])
+    total = np.sum(ground_truth)
+    
+    precision = TP/(TP+FP)
+    recall = TP/(TP+FN)
+    f1 = 2*(precision*recall)/(precision+recall)
+    accuracy = correct/total
+    
+    return precision, recall, f1, accuracy
+
+
+def multiclass_accuracy(prediction, ground_truth):
+    '''
+    Computes metrics for multiclass classification
+
+    Arguments:
+    prediction, np array of int (num_samples) - model predictions
+    ground_truth, np array of int (num_samples) - true labels
+
+    Returns:
+    accuracy - ratio of accurate predictions to total samples
+    '''
+    # TODO: Implement computing accuracy
+    return 0
